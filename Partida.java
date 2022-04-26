e3e3410052abcb7d58c4d1e98702a381b829a322
Piroliro <srpiroliro21@gmail.com>
2022-04-26 09:35:49 +0200

    optimitzat

--- Partida.java
+++ Partida.java
@@ -15,7 +15,6 @@ public class Partida {
     }
 
     public static void main(String[] args) {
-
         Scanner scan=new Scanner(System.in);
         // ordenar de + dificil a -
         Collections.shuffle(preguntas);
@@ -31,36 +30,26 @@ public class Partida {
         System.out.println("Una vez mezcladas responde Verdadero (v) o  Falso (f) a las siguientes afirmaciones");
 
         for (int i=0; i<preguntas.size();i++) {
-            System.out.println( i + ". " + preguntas.get(i).getAfirmacio());
+            System.out.println( i + ". " + preguntas.get(i).getEnunciat());
 
             System.out.println("Verdadero (v) o falso(f)?");
-            resposta=scan.nextLine();
+            resposta=scan.nextLine().toLowerCase();
 
-            respostt
-
-            if (resposta.toLowerCase().equals("v")) {
-                if (preguntas.get(i).getValue() == true) {
-
-                    System.out.println("Correcto");
-                    num_correcte++;
-                    score+=preguntas.get(i).getScore();
-
-                } else {
-                    System.out.println("Incorrecto");
-                }
-            } else if (resposta.toLowerCase().equals("f")) {
-                if ( preguntas.get(i).getValue() == false) {
-                    System.out.println("Correcto");
-                    num_correcte++;
-                    score += preguntas.get(i).getScore();
-                } else {
-                    System.out.println("Incorrecto");
-                }
-            }
+            if (preguntas.get(i).getValue()==resposta.equals("v")) {
+                System.out.println("Correcto");
+                score+=preguntas.get(i).getScore(); num_correcte++;
+            } else System.out.println("Incorrecto");
+            
             System.out.println("");
         }
-        System.out.println(" Has respondido correctamente a " + num_correcte );
-        System.out.println(" Has obtenido " + score + " puntos");
-        System.out.println(" El porcentage de aciertos es de  " + (num_correcte / preguntas.size()*100) );
+        
+    }
+
+    private void stats() {
+        System.out.println(
+            "Total de respostes encertades: "+num_correcte+"\n"+
+            "Puntuació obtinguda: "+score+"\n"+
+            "Tant per cent de respostes encertades respecte del total: "+(num_correcte / preguntas.size()*100)+"%"
+        );
     }
 }
