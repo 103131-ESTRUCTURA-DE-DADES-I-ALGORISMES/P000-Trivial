f96761d12bc929c845680243ae00bff4c9d13594
rems <srpiroliro21@gmail.com>
2022-04-26 08:46:04 +0200

    First Commit

---
+++ Pregunta.java
@@ -0,0 +1,15 @@
+public class Pregunta {
+
+	String afirmacio;
+	boolean cert;
+	int dificultat;
+	
+	public Pregunta(String afirmacio, boolean cert, int dificultat){
+		this.afirmacio=afirmacio;
+		this.cert=cert;
+		this.dificultat=dificultat;
+	}
+	public String getAfirmacio(){ return this.afirmacio; }
+	public boolean getCert() { return this.cert; }
+	public int getDificultat() { return this.dificultat; }
+}
